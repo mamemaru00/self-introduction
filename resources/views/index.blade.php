82523b17c1b24a5b8e32d43ac5432bffb3f2ebbe
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-24 00:28:54 +0900

    紹介文

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自己紹介</title>
+</head>
+<body>
+    <div class="heder">
+        <p>PROFILE</p>
+        <div class="photo"><a href="#"></a></div>
+    </div>
+    <div class="content">
+        <div class="self">
+            <p>自己紹介</p>
+            <h1>
+                こんにちは！SHOUHEIです！
+                <br>現在は主にPHPを使用したシステムエンジニアとして働いています！
+                <br>より多くの知見と経験を得るために個人開発を進めています！
+                <br>何卒よろしくお願いします！！！
+            </h1>
+        </div>
+    </div>
+</body>
+</html>
