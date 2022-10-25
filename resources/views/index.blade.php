9eac32f9b23342582b00b72ff05b22429954df21
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-25 23:41:20 +0900

    目次

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
     <title>自己紹介</title>
 </head>
 <body>
+    <div class="toggle">
+        <p>自己紹介</p>
+        <p>趣味</p>
+        <p>経歴</p>
+        <p>成果物</p>
+    </div>
+    <div class="social">
+        <a href="#">ツイッター</a>
+        <a href="#">メール</a>
+    </div>
     <div class="heder">
         <p>PROFILE</p>
         <div class="photo"><a href="#"></a></div>
