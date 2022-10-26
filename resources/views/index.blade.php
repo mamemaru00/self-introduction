aa8f914d0bd97041d6238a32440ef3f10d4d2f0e
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-27 00:15:03 +0900

    途中

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
+    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}">
     <title>自己紹介</title>
 </head>
+
 <body>
     <div class="toggle">
-        <p>自己紹介</p>
-        <p>趣味</p>
-        <p>経歴</p>
-        <p>成果物</p>
+        <p class="big">自己紹介</p>
+        <p class="big">趣味</p>
+        <p class="big">経歴</p>
+        <p class="big">成果物</p>
+        <!-- <div class="social">
+            <a href="#">ツイッター</a>
+            <a href="#">メール</a>
+        </div> -->
     </div>
     <div class="social">
         <a href="#">ツイッター</a>
@@ -20,18 +26,39 @@
     </div>
     <div class="heder">
         <p>PROFILE</p>
-        <div class="photo"><a href="#"></a></div>
+        <div class="photo">
+            <img class="photo_size" style="width:15rem; height:12rem; border-radius: 50%;" src="/images/log1.jpg">
+        </div>
     </div>
     <div class="content">
         <div class="self">
-            <p>自己紹介</p>
-            <h1>
+            <h1 style="text-align: center;">自己紹介</h1>
+            <h2>
                 こんにちは！SHOUHEIです！
                 <br>現在は主にPHPを使用したシステムエンジニアとして働いています！
                 <br>より多くの知見と経験を得るために個人開発を進めています！
                 <br>何卒よろしくお願いします！！！
-            </h1>
+            </h2>
+        </div>
+        <div class="self">
+            <h1 style="text-align: center;">趣味</h1>
+            <div class="hobby">読書</div>
+        </div>
+        <div class="self">
+            <h1 style="text-align: center;">経歴</h1>
+            <h2>
+                新卒で自社開発企業に就職する。
+                <br>1年半で見切りをつけてベンチャー企業に転職するも、
+                <br>圧力に耐えられずうつ病になる。
+                <br>その後1ヶ月の休職を経て復帰するも、うつ病が再発して退職する。
+                <br>知人の企業にSESとして就職。現在に至る。
+            </h2>
+        </div>
+        <div class="self">
+            <h1 style="text-align: center;">成果物</h1>
+            
         </div>
     </div>
 </body>
+
 </html>
